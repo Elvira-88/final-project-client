8b52e67e43814f8dbe3334e6fdc9e6710dc893f1
elvira <elvipt88@gmail.com>
2021-06-23 13:59:48 +0200

    Rellenando home

--- src/pages/hire-course/hireCourse.js
+++ src/pages/hire-course/hireCourse.js
@@ -3,7 +3,21 @@ import React from 'react'
 export default function hireCourse() {
     return (
         <div>
-            
+            <div>   
+                <h4>Método de pago</h4>        
+                <div>
+                    <input type="radio" name="paymentMethod" id="paypalInput"></input>
+                    <label for="paypalInput">Paypal</label>
+                </div>
+                <div>
+                    <input type="radio" name="paymentMethod" id="creditCardInput" checked></input>
+                    <label for="creditCardInput">Tarjeta de crédito</label>
+                </div>
+                <div>
+                    <input type="radio" name="paymentMethod" id="bizumInput" required></input>
+                    <label for="bizumInput">Bizum</label>
+                </div>              
+            </div>  
         </div>
     )
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -7,24 +7,29 @@ export default function home() {
         
         <div className="home">
             <div className="textUp">
-                <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error magnam quisquam deserunt, vero laborum, non maxime consectetur alias aliquam numquam nemo! Qui fugiat non totam rerum tenetur, cum voluptates cupiditate est fuga officia nemo natus id porro exercitationem rem quod ea ipsam accusantium quo sequi eligendi asperiores? Ut, expedita quam!</p>
+                <p className="paragraph">Formatio es una escuela de negocios que apuesta por un modelo de aprendizaje moderno, flexible y adaptativo capaz de responder a las necesidades reales de las empresas y los profesionales que vivimos en una sociedad actual sometida a continuas transformaciones y constantes cambios. Nuestro objetivo es formar los nuevos líderes que demanda el mundo en el que vivimos; capaces de emprender, innovar y crear valor a sus compañías, sus empleados y a la sociedad de forma sostenible y rentable. </p>
             </div>
             <div className="homeCenter">
                 <div className="textLeft">
-                    <p>¿Quieres mejorar el rendimiento de tu empresa?</p>
-                    <p>¿Necesitas formación?</p>
+                    <h3>¿Qué te proponemos?</h3>
+                    <h4>Aprendizaje real</h4>
+                    <p>Enfréntate a casos prácticos en los que aplicarás la materia aprendida y que te permitirán enfrentarte a situaciones que se darán en tu día a día en tu trabajo. Mediante simulación y role playing, aprenderás a resolver problemas y a tomar decisiones empresariales por tí mismo</p>
+                    <h4>Programas continuamente actualizados</h4>
+                    <p>En una sociedad en la que el conocimiento evoluciona rápidamente, la enseñanza ha de hacerlo al mismso ritmo. Por eso, nuestros programas se actualizan en cada convocatoria, para que el alumno tenga acceso al aprendizaje más innovador, actualice sus conocimientos y se recicle como profesional.</p>
+                    <h4>Profesores que son emprendedores de éxito</h4>
+                    <p>Nuestros profesores son profesionales especializados en activo que conocen la realidad de las empresas, el entorno de trabajo y el mercado laboral. Están capacitados para transmitir ese conocimiento y experiencia a sus alumnos.</p>
                 </div> 
                 <div className="principalImg">
                     <img src="https://srvsbniisphp-universitatpolit.netdna-ssl.com/cdn_propintegral/sites/default/files/styles/ancho680px/public/noticia/66106/field_foto/gettyimages-1181607218.jpg" alt="Foto"/>
                 </div>
                 <div className="textRight">
                     <p>¿Quieres mejorar el rendimiento de tu empresa?</p>
-                    <p>¿Necesitas formación?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed adipisci voluptatibus, quos dolorum natus alias porro nostrum soluta eligendi nisi laudantium? Unde molestias laborum totam nam, cupiditate et illo enim necessitatibus, autem iure quia aut dicta quod non architecto fuga facilis cumque maiores voluptates? Voluptate dolorum quae nam rem quo.</p>
                 </div>  
             </div>            
             <div className="textDown">
                 <p className="paragraph">Texto abajo</p>
-            </div>    
+            </div>                   
             
         </div>
         
